a9eed15f0f37118b00c4c010dbc2acb8aa1569be
tkmct <1220t.takamichi@gmail.com>
2019-05-26 15:19:00 +0900

    Add wasm crate

---
+++ wasm/src/lib.rs
@@ -0,0 +1,20 @@
+mod utils;
+
+use bytes::Bytes;
+use ethereum_types::Address;
+use plasma_core::data_structure::state_object::StateObject;
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn new_state_object(param: &JsValue) {
+    if let Some(v) = param.as_string() {
+        let state_object = StateObject::new(Address::zero(), &Bytes::from(v));
+        let _encoded = rlp::encode(&state_object);
+    };
+}
